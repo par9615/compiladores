aebb4ebd127380cded9d63e78ef203068f818d06
kireck211 <erickdesantiago11@gmail.com>
2017-04-14 19:37:17 -0500

    Parsing grammar with lexicalAnalyzer module

---
+++ ascendantAnalysis/ascendantAnalyzer.py
@@ -0,0 +1,194 @@
+################# Global variables #################
+stack = []
+####################################################
+inputString = ""
+tokens = []
+symbols = []
+terminals = ["a", "b", "c"]
+nonTerminals = []
+initial = 0
+
+matrix = {
+    0 : {
+        "a":   "S2",
+        "S" :   "G1"
+    },
+
+    1: {
+        "$":    "AC"
+    },
+
+    2: {
+        "b":    "S4",
+        "c":    "R2",
+        "$":    "R2",
+        "A":    "G3"
+    },
+
+    3: {
+        "c":    "R1",
+        "$":    "R1"
+    },
+
+    4: {
+        "b":   "S5"
+    },
+
+    5: {
+        "a":    "S2",
+        "S":    "G6"
+    },
+
+    6: {
+        "c":   "S7"
+    },
+
+    7: {
+        "c":   "R3",
+        "$" :   "R3"
+    }
+}
+
+grammar = {
+        1: ("S", ["a","A"]),
+        2:  ("A",[]),
+        3: ("A",["b","b","S","c"])
+}
+
+follow = {
+    "S": ["$", "c"],
+    "A": ["$", "c"]
+}
+
+def getTokens(inputString):
+    i = 0
+    while (i < len(inputString)):
+        character = inputString[i]
+        if (character == ' '):
+            i+=1
+        for terminal in terminals:
+            count = 0
+            for letter in terminal:
+                if (i < len(inputString) and letter == inputString[i]):
+                    i += 1
+                    count += 1
+                else:
+                    break
+            if (count == len(terminal)):
+                tokens.append(terminal)
+                break
+    tokens.append('$')
+
+def pushNextState(top, token):  #Hace un push en la pila del estado en matrix[top][token] -> "S5" hace stack.append(5)
+    global stack
+    state = getState(top,token)
+    stack.append(state)
+
+def shift(top, token):   #No hace nada
+    global stack
+
+def reduce(top, token): #Hace todo el procedimiento del reduce
+    global stack
+    global symbols
+
+    rule  = grammar[getState(top,token)][1]
+    head = grammar[getState(top,token)][0]
+
+    for i in range(0, len(rule)):
+        stack.pop()
+        symbols.pop()
+
+    pushNextState(stack[-1], head)
+    symbols.append(head)
+
+    return [head, "".join(rule)]
+
+def action(top, token):             #Retorna la acción en matrix[top][token] -> "S5" return "S"
+    action = matrix[top][token]
+    return action
+
+def getState(top, token):           #Retorna el estado en matrix[top][token] -> "S5" return int(5)
+    return int(matrix[top][token][1:])
+
+def handleError(top):
+    global stack
+    global follow
+    global tokens
+    global symbols
+
+    A = "S"
+    inputToken = tokens[0]
+    s = top
+
+    while(not(A in matrix[s])):
+        stack.pop()
+        s = stack[-1]
+
+    while(not(inputToken in follow[A])):
+        tokens.pop(0)
+        inputToken = tokens[0]
+
+
+    stack.append(getState(s,A))
+
+    symbols.append(A)
+
+
+
+
+####################################
+
+inputString = input("Escriba entrada \n>")
+
+getTokens(inputString)
+
+stack.append(initial)
+
+formattedString = "{:>15}\t{:>15}\t{:>15}\t{:>15}"
+
+print(formattedString.format("Pila", "Simbolos", "Entrada", "Acción"))
+output = []
+token = tokens[0]
+
+while(1):
+    output.append(" ".join(map(str,stack)))
+
+    output.append("".join(symbols))
+
+    output.append("".join(tokens))
+
+    top = stack[-1]
+
+    if(token in matrix[top]):
+        act = action(top, token)
+
+        if("S" in act): #Shift
+            pushNextState(top,token)
+            symbols.append(token)
+            tokens.pop(0)
+            token = tokens[0]
+            output.append("Shift " + act[1:])
+
+        elif("R" in act): #Reduce
+            rule = reduce(top,token)
+            output.append("Reduce " + rule[0] + " -> " + rule[1])
+
+        elif("A" in act): #Aceptado
+            output.append("Aceptado")
+
+        else:
+            handleError(top)
+            token = tokens[0]
+            output.append("Error T")
+
+    else:
+        handleError(top)
+        token = tokens[0]
+        output.append("Error T")
+
+    print(formattedString.format(output[0], output[1], output[2], output[3]))
+
+    if (output[-1] == "Aceptado"):
+        break;
+
+    del output[:]
---
+++ ascendantAnalysis/grammar.py
@@ -0,0 +1,173 @@
+from compiladores.lexicalAnalysis.lexicalAnalyzer import *
+rules = {
+	'Sx' : {
+		'A_aug;Sx' : 0,
+		'A_sim;Sx' : 1,
+		'A_dro;Sx' : 2,
+		'A_aug;' : 3,
+		'A_sim;' : 4,
+		'A_dro;' : 5,
+		'CtrlSx' : 6,
+		'Ctrl' : 7
+	},
+	'A_aug' : {
+		'IDAoEx': 8
+	},
+	'A_sim' : {
+		'ID=Ex' : 9
+	},
+	'A_dro' : {
+		'IDDc' : 10
+	},
+	'Ao' : {
+		'+=' : 11,
+		'-=' : 12,
+		'*=' : 13,
+		'/=' : 14,
+		'%=' : 15,
+		'>>=' : 16,
+		'<<=' : 17,
+		'&=' : 18,
+		'|=' : 19,
+		'^=' : 20,
+		'**=' : 21
+	},
+	'Ex' : {
+		'Or_l' : 22
+	},
+	'Or_l' : {
+		'Or_l||And_l' : 23,
+		'And_l' : 24
+	},
+	'And_l' : {
+		'And_l&&Not_l' : 25,
+		'Not_l' : 26
+	},
+	'Not_l' : {
+		'!Not_l' : 27,
+		'Lx' : 28
+	},
+	'Lx' : {
+		'LxLoOr_b' : 29,
+		'Or_b' : 30
+	},
+	'Lo' : {
+		'<' : 31,
+		'>' : 32,
+		'<=' : 33,
+		'>=' : 34,
+		'==' : 35,
+		'!=' : 36
+	},
+	'Or_b' : {
+		'Or_b|Xor_b' : 37,
+		'Xor_b' : 38
+	},
+	'Xor_b' : {
+		'Xor_b^And_b' : 39,
+		'And_b' : 40
+	},
+	'And_b' : {
+		'And_b&Shift' : 41,
+		'Shift' : 42
+	},
+	'Shift': {
+		'Shift>>Ax' : 43,
+		'Shift<<Ax' : 44,
+		'Ax' : 45
+	},
+	'Ax' : {
+		'Ax+Af' : 46,
+		'Ax-Af' : 47,
+		'Af' : 48
+	},
+	'Af' : {
+		'Af*Ap' : 49,
+		'Af%Ap' : 50,
+		'Af/Ap' : 51,
+		'Ap' : 52
+	},
+	'Ap' : {
+		'At**Ap' : 53,
+		'At' : 54
+	},
+	'At' : {
+		'SiNUMBER' : 55,
+		'STRING' : 56,
+		'SiID' : 57,
+		'Si(Ex)' : 58
+	},
+	'Si' : {
+		'+' : 59,
+		'-' : 60,
+		'~' : 61,
+		'#' : 62
+	},
+	'Dc' : {
+		'=**(STRING,NUMBER)**' : 63,
+		'-<(Ex,Ex,Ex)' : 64,
+		'-)(Ex,Ex,Ex)' : 65
+	},
+	'Ctrl' : {
+		'For' : 66,
+		'While' : 67,
+		'If' : 68
+	},
+	'For' : {
+		'for(ID:(Ex,Ex)){Sx}' : 69
+	},
+	'While' : {
+		'while(Ex){Sx}' : 70
+	},
+	'If' : {
+		'if(Ex){Sx}' : 71,
+		'if(Ex)else{Sx}' : 72
+	}
+}
+
+class Singleton(type):
+	_instances = {}
+	def __call__(cls, *args, **kwargs):
+		if cls not in cls._instances:
+			cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+		return cls._instances[cls]
+
+class Grammar(object,grammarPattern):
+	__metaclass__ = Singleton
+
+	def __init__(self):
+		self.grammar = {}
+		tokenizer = Token(grammarPattern)
+		for rule in rules:
+			for production in rules[rule]:
+				indexRule = rules[rule][production]
+				self.grammar[indexRule] = (rule, tokenizer.tokenizeText(production))
+
+
+grammarPattern = r"""
+(?P<droneDeclarationStart>=\*\*\()
+|(?P<droneDelcarationEnd>\)\*\*)
+|(?P<moveDeclaration>-<)
+|(?P<rotateDeclaration>-\))
+|(?P<leftCurly>[{])
+|(?P<rightCurly>[}])
+|(?P<leftBracket>[(])
+|(?P<rightBracker>[)])
+|(?P<non-terminal>(Sx|A_aug|A_sim|A_dro|Ao|Ex|Or_l|And_l|Not_l|Lx|Lo|Or_b|Xor_b|And_b|Shift|Ax|Af|Ap|At|Si|Dc|Ctrl|For|While|If) )
+|(?P<endStatement>[;])
+|(?P<identifier>ID)
+|(?P<updater>(\+=|-=|\*=|/=|%=|>>=|<<=|&=|\|=|\^=|\*\*=))
+|(?P<comparatorLarger>(<=|>=|==|!=))
+|(?P<operatorLarger>(\|\| | && | \*\*))
+|(?P<operator>(\+ | - | ~ | \* | % | / | >> | << | & | \| | \^ | !) )
+|(?P<number>NUMBER)
+|(?P<string>STRING)
+|(?P<epsilon>#)
+|(?P<reservedWord>(while|if|for))
+|(?P<whitespace>\s+)
+|(?P<twoDots>[:])
+"""
+
+
+grammar = Grammar()
+print(grammar.grammar)
