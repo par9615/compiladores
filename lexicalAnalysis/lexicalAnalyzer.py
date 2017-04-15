5f3ec95a2d34c1eee04095fd6f78c8b79bc3f0fd
kireck211 <erickdesantiago11@gmail.com>
2017-04-14 19:38:35 -0500

    Refactoring directories names

---
+++ lexicalAnalysis/lexicalAnalyzer.py
@@ -0,0 +1,23 @@
+import re
+
+class TokenizerException(Exception): pass
+
+class Token(object,pattern):
+    def __init__(self): 
+        token_re = re.compile(pattern, re.VERBOSE)
+
+    def getTokens(self, text):
+        return tokenizeText(text)
+
+    def tokenizeText(self, text)
+        pos = 0
+        while True:
+            m = token_re.match(text, pos)
+            if not m: break
+            pos = m.end()
+            tokname = m.lastgroup
+            tokvalue = m.group(tokname)
+            yield tokname, tokvalue
+        if pos != len(text):
+            raise TokenizerException('tokenizer stopped at pos %r of %r' % (
+                pos, len(text)))
